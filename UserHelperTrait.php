21b88781a30dae523b1980df2594dd4bc6aface7
Giandomenico Olini <giando.olini@gogodigital.it>
2017-06-05 23:47:17 +0200

    Update UserHelper

--- UserHelperTrait.php
+++ UserHelperTrait.php
@@ -33,4 +33,45 @@ trait UserHelperTrait
         return $user['id'];
     }
 
+    /**
+     * Return User's array with current User on first position [ 'user_id' => 'username' ]
+     *
+     * @param $user_id
+     * @param $username
+     * @return array
+     */
+    public function getUsersSelect2($user_id,$username)
+    {
+        $users = User::find()
+            ->select(['id','username'])
+            ->where(['blocked_at' => null, 'unconfirmed_email' => null])
+            ->andWhere(['!=', 'id', $user_id])
+            ->all();
+
+        $array[$user_id] = ucwords($username);
+
+        foreach($users as $user) {
+            $array[$user['id']] = ucwords($user['username']);
+        }
+
+        return $array;
+    }
+
+    /**
+     * Return an array with the User's Roles
+     *
+     * @return array
+     */
+    public function getRoles()
+    {
+        $roles = \Yii::$app->authManager->getRoles();
+        $array = ['public' => 'Public'];
+
+        foreach($roles as $role) {
+            $array[ucwords($role->name)] = ucwords($role->name);
+        }
+
+        return $array;
+    }
+
 }

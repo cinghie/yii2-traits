1e3804fad79e08c760ab23179f910cef72e6fdc0
Giandomenico Olini <giando.olini@gogodigital.it>
2017-06-16 21:37:28 +0200

    Adding class Migration

---
+++ migrations/Migration.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-traits
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-traits
+ * @version 0.1.0
+ */
+
+namespace cinghie\traits\migrations;
+
+use Yii;
+
+class Migration extends \yii\db\Migration
+{
+
+    /**
+     * @var string
+     */
+    protected $tableOptions;
+	
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        
+		switch (Yii::$app->db->driverName) 
+		{
+            case 'mysql':
+                $this->tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+                break;
+            case 'pgsql':
+                $this->tableOptions = null;
+                break;
+            default:
+                throw new \RuntimeException('Your database is not supported!');
+        }
+    }
+
+}

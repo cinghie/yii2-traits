683f5ddae1dc432ba338fa6a3cca0ba9bec398a7
Giandomenico Olini <giando.olini@gogodigital.it>
2017-06-16 21:36:25 +0200

    Adding copyright

--- AccessTrait.php
+++ AccessTrait.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-traits
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-traits
+ * @version 0.1.0
+ */
+
 namespace cinghie\traits;
 
 use kartik\detail\DetailView;

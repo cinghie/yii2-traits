487a7db7c038353b9d75e305c8545e59d2d2aee5
Giandomenico Olini <giando.olini@gogodigital.it>
2017-06-18 01:56:16 +0200

    Adding ImageTrait

---
+++ ImageTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-traits
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-traits
+ * @version 0.1.0
+ */
+
+namespace cinghie\traits;
+
+use Yii;
+
+/**
+ * Trait ImageTrait
+ *
+ * @property string $title
+ */
+trait ImageTrait
+{
+
+
+
+}

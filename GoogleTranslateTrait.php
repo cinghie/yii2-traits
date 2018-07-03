7ea3458e4c4f84bcd01fead68a313f6d8e930904
Giando <giando.olini@gogodigital.it>
2018-07-03 22:47:58 +0200

    Update GoogleTranslateTrait

--- GoogleTranslateTrait.php
+++ GoogleTranslateTrait.php
@@ -12,10 +12,35 @@
 
 namespace cinghie\traits;
 
+use Google\Cloud\Translate\TranslateClient;
+
 /**
  * Trait GoogleTranslateTrait
  */
 trait GoogleTranslateTrait
 {
+
+	/**
+	 * Get Translation from Google Cloud Translate
+	 *
+	 * @param string $apiKey
+	 * @param string $text
+	 * @param string $lang
+	 *
+	 * @return string
+	 */
+	public function getGoogleCloudTranslation($apiKey,$text,$lang)
+	{
+		// Instantiates a client
+		$translate = new TranslateClient([
+			'Key' => $apiKey,
+			'targetLanguage' => $lang
+		]);
+
+		# Translates some text into Russian
+		$translation = $translate->translate($text);
+
+		return $translation['text'];
+	}
 
 }

957e9b3c9b59d7622a5e4cff861c720145165c50
Giandomenico Olini <giando.olini@gogodigital.it>
2018-07-03 18:06:11 +0200

    Adding GoogleTranslateTrait

---
+++ GoogleTranslateTrait.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Gogodigital Srls
+ * @company Gogodigital Srls - Wide ICT Solutions
+ * @website http://www.gogodigital.it
+ * @github https://github.com/cinghie/yii2-traits
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @package yii2-traits
+ * @version 1.2.0
+ */
+
+namespace cinghie\traits;
+
+/**
+ * Trait GoogleTranslateTrait
+ */
+trait GoogleTranslateTrait
+{
+
+}

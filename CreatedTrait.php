f6deb4e2628f1015641596d4c19ebb56fcaaa342
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-18 14:12:42 +0200

    Delegate created audit UI concerns

--- CreatedTrait.php
+++ CreatedTrait.php
@@ -1,34 +1,17 @@
 <?php
 
-/**
- * @copyright Copyright &copy; Gogodigital Srls
- * @company Gogodigital Srls - Wide ICT Solutions
- * @website http://www.gogodigital.it
- * @github https://github.com/cinghie/yii2-traits
- * @license GNU GENERAL PUBLIC LICENSE VERSION 3
- * @package yii2-traits
- * @version 1.2.3
- */
-
 namespace cinghie\traits;
 
-use Exception;
 use Yii;
+use cinghie\traits\ui\AuditUi;
 use dektrium\user\models\User;
-use kartik\detail\DetailView;
-use kartik\form\ActiveField;
-use kartik\helpers\Html;
-use kartik\widgets\ActiveForm;
-use kartik\widgets\DateTimePicker;
-use kartik\widgets\Select2;
-use yii\base\InvalidParamException;
-use yii\base\Model;
-use yii\db\ActiveQuery;
 use yii\db\ActiveRecord;
-use yii\helpers\Url;
 
 /**
  * Trait CreatedTrait
+ *
+ * Model rules, relations and authorization checks remain here; presentation
+ * is delegated to ui/AuditUi.
  *
  * @property string $created
  * @property int $created_by
@@ -47,11 +30,7 @@ trait CreatedTrait
 
     public function getCreatedRules()
     {
-        return [
-            [['created'], 'safe'],
-            [['created_by'], 'integer'],
-            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
-        ];
+        return static::rules();
     }
 
     public static function attributeLabels()
@@ -64,10 +43,7 @@ trait CreatedTrait
 
     public function getCreatedAttributeLabels()
     {
-        return [
-            'created' => Yii::t('traits', 'Created'),
-            'created_by' => Yii::t('traits', 'Created By'),
-        ];
+        return static::attributeLabels();
     }
 
     public function getCreatedBy()
@@ -79,18 +55,7 @@ trait CreatedTrait
     public function getCreatedWidget($form)
     {
         $created = $this->isNewRecord ? date('Y-m-d H:i:s') : $this->created;
-
-        /** @var $this Model */
-        return $form->field($this, 'created')->widget(DateTimePicker::class, [
-            'options' => [
-                'value' => $created,
-            ],
-            'pluginOptions' => [
-                'autoclose'      => true,
-                'format'         => 'yyyy-mm-dd hh:ii:ss',
-                'todayHighlight' => true,
-            ]
-        ]);
+        return AuditUi::dateTimeWidget($this, $form, 'created', $created);
     }
 
     public function getCreatedDetailView()
@@ -101,57 +66,30 @@ trait CreatedTrait
     public function getCreatedByWidget($form)
     {
         if ($this->isNewRecord) {
-            $createdBy = $this->getCurrentUserSelect2();
+            $data = $this->getCurrentUserSelect2();
         } elseif ($this->created_by && $this->createdBy) {
-            $createdBy = [$this->created_by => $this->createdBy->username];
+            $data = [$this->created_by => $this->createdBy->username];
         } else {
-            $createdBy = [];
+            $data = [];
         }
 
-        /** @var $this Model */
-        return $form->field($this, 'created_by')->widget(Select2::class, [
-            'data' => $createdBy,
-            'addon' => [
-                'prepend' => [
-                    'content'=>'<i class="fa fa-user"></i>'
-                ]
-            ]
-        ]);
+        return AuditUi::userWidget($this, $form, 'created_by', $data);
     }
 
     public function getCreatedByGridView()
     {
-        $createdBy = $this->createdBy;
-        if ($this->created_by && $createdBy) {
-            $url = urldecode(Url::toRoute(['/user/profile/show', 'id' => $this->created_by]));
-            return Html::a($createdBy->username, $url);
-        }
-
-        return Yii::t('traits', 'Nobody');
+        return AuditUi::userGridValue($this->created_by, $this->createdBy);
     }
 
     public function getCreatedByDetailView()
     {
-        $createdBy = $this->createdBy;
-
-        return [
-            'attribute' => 'created_by',
-            'format' => 'html',
-            'type' => DetailView::INPUT_SWITCH,
-            'value' => $this->created_by && $createdBy
-                ? Html::a($createdBy->username, urldecode(Url::toRoute(['/user/admin/update', 'id' => $this->created_by])))
-                : Yii::t('traits', 'Nobody'),
-            'valueColOptions'=> [
-                'style'=>'width:30%'
-            ]
-        ];
+        return AuditUi::userDetailView('created_by', $this->created_by, $this->createdBy);
     }
 
     public function isCurrentUserCreator()
     {
         /** @var User|null $currentUser */
         $currentUser = Yii::$app->user->identity;
-
         return $currentUser !== null && $currentUser->id === $this->created_by;
     }
 

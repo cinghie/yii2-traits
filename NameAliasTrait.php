d7541d6bd907f42e8a0dc39d29fadf39a612cab8
Giandomenico Olini <giando.olini@gogodigital.it>
2026-08-18 17:44:00 +0200

    Use Yii2-compatible trait rule helpers

--- NameAliasTrait.php
+++ NameAliasTrait.php
@@ -27,25 +27,6 @@ use yii\base\Model;
  */
 trait NameAliasTrait
 {
-    /**
-     * @inheritdoc
-     * 
-     * Note: In PHP 8.1+, calling this method statically (e.g., NameAliasTrait::rules())
-     * may generate a deprecation warning. It's recommended to use getNameAliasRules() instance method instead.
-     */
-    public static function rules()
-    {
-        return [
-            [['alias'], 'unique'],
-            [['alias','name'], 'string', 'max' => 255],
-        ];
-    }
-
-    /**
-     * Instance method to get rules without deprecation warning
-     * 
-     * @return array
-     */
     public function getNameAliasRules()
     {
         return [
@@ -54,29 +35,6 @@ trait NameAliasTrait
         ];
     }
 
-    /**
-     * @inheritdoc
-     * 
-     * Note: In PHP 8.1+, calling this method statically (e.g., NameAliasTrait::attributeLabels())
-     * will generate a deprecation warning. It's recommended to call this as an instance method
-     * in your model's attributeLabels() method instead.
-     * 
-     * @return array
-     */
-    public static function attributeLabels()
-    {
-        return [
-            'alias' => Yii::t('traits', 'Alias'),
-            'name' => Yii::t('traits', 'Name'),
-        ];
-    }
-
-    /**
-     * Instance method to get attribute labels without deprecation warning
-     * Use this method in your model's attributeLabels() instead of calling the static method
-     * 
-     * @return array
-     */
     public function getNameAliasAttributeLabels()
     {
         return [
@@ -85,13 +43,6 @@ trait NameAliasTrait
         ];
     }
 
-	/**
-	 * Generate alias from name
-	 *
-	 * @param string $name
-	 *
-	 * @return string
-	 */
 	public function generateAlias($name)
 	{
 		$slugifyOptions = Yii::$app->controller->module->slugifyOptions ?? [
@@ -108,14 +59,6 @@ trait NameAliasTrait
 		return $slugify->slugify($name);
 	}
 
-	/**
-	 * Set alias from post
-	 *
-	 * @param [] $post
-	 * @param string $field
-	 *
-	 * @return void
-	 */
 	public function setAlias($post,$field)
 	{
 		$slugifyOptions = Yii::$app->controller->module->slugifyOptions ?? [
@@ -134,33 +77,13 @@ trait NameAliasTrait
 		}
 	}
 
-	/**
-	 * Purge alias by string
-	 *
-	 * @param string $string
-	 *
-	 * @return string
-	 */
 	public function purgeAlias($string)
 	{
-		// remove any '-' from the string they will be used as concatonater
 		$string = str_replace(array('-','_'), ' ', $string);
-
-		// remove any duplicate whitespace, and ensure all characters are alphanumeric
 		$string = preg_replace(array('/\s+/','/[^A-Za-z0-9\-]/'), array('-',''), $string);
-
-		// lowercase and trim
-		return strtolower( trim( $string ) );
+		return strtolower(trim($string));
 	}
 
-	/**
-	 * Generate Name Form Widget
-	 *
-	 * @param ActiveForm $form
-	 *
-	 * @return ActiveField
-	 * @throws InvalidConfigException
-	 */
     public function getNameWidget($form)
     {
         /** @var $this Model */
@@ -173,14 +96,6 @@ trait NameAliasTrait
         ])->textInput(['maxlength' => true]);
     }
 
-	/**
-	 * Generate alias Form Widget
-	 *
-	 * @param ActiveForm $form
-	 *
-	 * @return ActiveField
-	 * @throws InvalidConfigException
-	 */
     public function getAliasWidget($form)
     {
         /** @var $this Model */
